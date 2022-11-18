9d22f0b6c552a748d0ead3ed8ee27be34333f443
POMXARK <1997pom@gmail.com>
2022-11-18 10:52:35 +1000

    Add project files.

---
+++ AvaloniaReativeUIPrismExempleEventAggregator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace AvaloniaReativeUIPrismExempleEventAggregator.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ AvaloniaReativeUIPrismExempleEventAggregator/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace AvaloniaReativeUIPrismExempleEventAggregator.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
