9abcccca420d2ffe82aafc2388b97b8051189db9
POMXARK <1997pom@gmail.com>
2022-11-18 11:57:05 +1000

    add prism event aggregator

--- AvaloniaReativeUIPrismExempleEventAggregator/App.axaml.cs
+++ AvaloniaReativeUIPrismExempleEventAggregator/App.axaml.cs
@@ -1,29 +1,54 @@
 using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using AvaloniaReativeUIPrismExempleEventAggregator.ViewModels;
 using AvaloniaReativeUIPrismExempleEventAggregator.Views;
+using Prism.DryIoc;
+using Prism.Events;
+using Prism.Ioc;
+using Prism.Regions;
 
 namespace AvaloniaReativeUIPrismExempleEventAggregator
 {
-    public partial class App : Application
+    public class App : PrismApplication
     {
+        // Note:
+        //  Though, Prism.WPF v8.1 uses, `protected virtual void Initialize()`
+        //  Avalonia's AppBuilderBase.cs calls, `.Setup() { ... Instance.Initialize(); ... }`
+        //  Therefore, we need this as a `public override void` in PrismApplicationBase.cs
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
+
+            // DON'T FORGET TO CALL THIS
+            base.Initialize();
         }
 
-        public override void OnFrameworkInitializationCompleted()
-        {
-            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-            {
-                desktop.MainWindow = new MainWindow
-                {
-                    DataContext = new MainWindowViewModel(),
-                };
-            }
 
-            base.OnFrameworkInitializationCompleted();
+        /// <summary>Called after Initialize.</summary>
+        protected override void OnInitialized()
+        {
+            // Register Views to the Region it will appear in. Don't register them in the ViewModel.
+            var regionManager = Container.Resolve<IRegionManager>();
+            //regionManager.RegisterViewWithRegion(RegionNames.ContentRegion, typeof(DashboardView));
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Views - Generic
+            containerRegistry.Register<MainWindow>();
+
+            containerRegistry.RegisterSingleton<IEventAggregator, EventAggregator>();
+            //containerRegistry.RegisterSingleton<ITwoWindowViewModel, TwoWindowViewModel>();
+
+            // Services
+
+            //containerRegistry.RegisterSingleton<INavigationViewModel, NavigationViewModel>();
+        }
+
+        /// <summary>User interface entry point, called after Register and ConfigureModules.</summary>
+        /// <returns>Startup View.</returns>
+        protected override IAvaloniaObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
         }
     }
 }
--- AvaloniaReativeUIPrismExempleEventAggregator/ViewModels/MainWindowViewModel.cs
+++ AvaloniaReativeUIPrismExempleEventAggregator/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,40 @@
-﻿namespace AvaloniaReativeUIPrismExempleEventAggregator.ViewModels
+﻿
+using Prism.Events;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+
+namespace AvaloniaReativeUIPrismExempleEventAggregator.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private IEventAggregator _eventAggregator;
+
         public string Greeting => "Welcome to Avalonia!";
+
+        [Reactive] public string Rx1 { get; set; }  
+
+
+        public MainWindowViewModel(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+            this.WhenAnyValue(x => x.Rx1)
+                .Subscribe(x => _eventAggregator.GetEvent<MessageSendEvent>().Publish(x));
+        }
     }
+
+    public class TwoWindowViewModel : ViewModelBase
+    {
+        private IEventAggregator _eventAggregator;
+
+        [Reactive] public string Rx2 { get; set; }
+
+        public TwoWindowViewModel(IEventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+            _eventAggregator.GetEvent<MessageSendEvent>().Subscribe(x => Rx2 = x); // принять изменение
+        }
+    }
+
+    public class MessageSendEvent : PubSubEvent<string>{}
 }
