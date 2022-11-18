d481775b9034b7e6cd0775f5f8909afa1115c64a
POMXARK <1997pom@gmail.com>
2022-11-18 10:59:44 +1000

    add windows

---
+++ AvaloniaReativeUIPrismExempleEventAggregator/Views/TwoWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaReativeUIPrismExempleEventAggregator.Views
+{
+    public partial class TwoWindow : UserControl
+    {
+        public TwoWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AvaloniaReativeUIPrismExempleEventAggregator/Views/UseCommandWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaReativeUIPrismExempleEventAggregator.Views
+{
+    public partial class UseCommandWindow : UserControl
+    {
+        public UseCommandWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
